7ed23ab02b9fab3b7b2bdd5637f00e533dc5b581
guinnod <210103064@stu.sdu.edu.kz>
2022-06-15 18:38:32 +0600

    gold done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import first from './media_src/photo-1443891238287-325a8fddd0f7.jpeg';
+import second from './media_src/flashlight.jpg';
+import third from './media_src/photo-1465188162913-8fb5709d6d57.jpeg';
+import fourth from './media_src/photo-1442522772768-9032b6d10e3e.jpeg';
+import fifth from './media_src/photo-1465156799763-2c087c332922.jpeg';
+import logo from './iq-logo.png';
+import { format } from 'date-fns';
 import './App.css';
+const today = new Date();
+const formattedDate = format(today, 'dd.MM.yyyy');
+const currentTime = new Date();
+const fCurrentTime = format(currentTime, 'hh:mm');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='page'>
+      <header>
+        <div className='lef'>
+        <img src={logo} className='logo'/>
+        <input></input>
+        </div>
+        <h2>{formattedDate} {fCurrentTime}</h2>
       </header>
+    <div className='main'>
+      <div className='imgr'>
+        <img src={first} className="imgs">
+        </img>
+        <h1>HEY</h1>
+      </div>
+      <div className='imgr'>
+        <img src={second} className="imgs">
+        </img>
+        <h1>LET`S</h1>
+      </div>
+      <div className='imgr'>
+        <img src={third} className="imgs">
+        </img>
+        <h1>GIVE</h1>
+      </div>
+      <div className='imgr'>
+        <img src={fourth} className="imgs">
+        </img>
+        <h1>ALL</h1>
+      </div>
+      <div className='imgr'>
+        <img src={fifth} className="imgs">
+        </img>
+        <h1>YOU CAN</h1>
+      </div>
+    </div>
     </div>
   );
 }
